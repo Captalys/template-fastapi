63c5b8fc3c3c18e12afce530b94ab4e36669dfd9
Fausto Alonso <fausto.alonso@captalys.com.br>
2020-09-21 17:35:55 -0300

    "Ajustada rota de hc"

--- {{cookiecutter.project_name}}/src/routes/healthcheck.py
+++ {{cookiecutter.project_name}}/src/routes/healthcheck.py
@@ -4,6 +4,6 @@ from fastapi.responses import JSONResponse
 router = APIRouter()
 
 
-@router.get("/api/healthcheck", status_code=200)
+@router.get("/healthcheck", status_code=200)
 def get_health():
     return JSONResponse(status_code=200, content=dict(message="I am alive."))
