17cdc6fe36fe6ec7d7480515abffbe7e77972828
Wanderson Ferreira <iagwanderson@gmail.com>
2020-09-21 09:32:41 -0300

    readme update url from repo

--- {{cookiecutter.project_name}}/src/resources/healthcheck.py
+++ {{cookiecutter.project_name}}/src/resources/healthcheck.py
@@ -3,7 +3,6 @@ from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
-
 @router.get("/healthcheck", status_code=200)
 def get_health():
     return JSONResponse(status_code=200, content=dict(message="I am alive."))
