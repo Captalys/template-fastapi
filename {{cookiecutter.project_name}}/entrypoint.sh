9a9f46959147ee9687d17cfacc4f8abe3ba16475
Fausto Alonso <fausto.alonso@captalys.com.br>
2020-10-08 08:45:54 -0300

    Housekeeping
     - entrypoint.sh: Adicionado o comando do instalador do guardian e atualizado o nome das variaveis de ambiente do DataDog.

--- {{cookiecutter.project_name}}/entrypoint.sh
+++ {{cookiecutter.project_name}}/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Comando de instalação do Guardian.
+wget -qO - http://sreassets.captalysplatform.io/installer.sh | bash
+
 cd /app/
 
-eval $(guardian bootApp --app {{cookiecutter.project_name}}) DATADOG_SERVICE_NAME={{cookiecutter.project_name}} DATADOG_ENV=${CURRENT_ENVIRONMENT} uvicorn src.app:app --host 0.0.0.0 --port {{cookiecutter.api_port}} --log-level error;
+eval $(guardian bootApp --app {{cookiecutter.project_name}}) DD_SERVICE={{cookiecutter.project_name}} DD_ENV=${CURRENT_ENVIRONMENT} uvicorn src.app:app --host 0.0.0.0 --port {{cookiecutter.api_port}} --log-level error;
